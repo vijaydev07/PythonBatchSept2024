71bb49493c949b9c9fde88738ecfd10da15041c2
VijayChimata <vijaykumarchimata07@gmail.com>
2024-10-08 21:11:34 +0000

    Arithmetic Operations

---
+++ 02_Basics/01_Arithmetic_Operations/a_arithmetic_operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Purpose: Arithmetic Operations
+
+    Integer Family
+        - int
+        - long -> only in python 2.x
+        - float
+
+        - complex
+        - bool
+"""
